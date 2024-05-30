f6bb035cf5fcae9192ef1afb7b7be1069e729347
Kibasa <miguelhmsantos21@gmail.com>
2024-05-30 19:29:48 -0300

    Done

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,17 +1,43 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { Fragment, useEffect } from 'react';
+import { fetchCartData, sendCartData } from './store/cart-actions';
+
+let isInitial = 2;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+
+    if(isInitial !== 0) {
+      isInitial -= 1;
+      return;
+    }
+
+    dispatch(sendCartData(cart))
+
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
